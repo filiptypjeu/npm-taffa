23c311c12a6051fababc54bef1f9a8928964bfb6
Filip Stenbacka <filip.stenbacka@trimble.com>
2019-11-30 00:02:31 +0200

    Added comments.

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,14 @@ export interface ITaffaMenu {
 
 const taffaBaseURL: string = 'http://api.tf.fi/taffa/';
 
+/**
+ * Reqeuests the menu for a specific day.
+ * 
+ * @param {Date | number | undefined} date - Specifies the date. Only menus one week into the future are usually available (or 5 weekdays). If this parameter is a number, it can only be 0-4, where 0 is today and 4 is four days into the future (not including Saturdays and Sundays). 0 is the default.
+ * @param {string | undefined} language - The languge the menu should be in. Can only be "sv" for Swedish, "fi" for Finnish and "en" for English. Swedish is default.
+ * 
+ * @returns {ITaffaMenu} An object containing the different lunch alternatives for the corresponding day. In addition, the object will also contain the date and name of the weekday.
+ */
 export const menu = async (date?: Date | number, language?: keyof typeof TaffaLanguageEnum): Promise<ITaffaMenu> => {
   let day = '0';
 
@@ -38,6 +46,14 @@ export const menu = async (date?: Date | number, language?: keyof typeof TaffaLa
     });
 };
 
+
+/**
+ * Requests the menus for a whole week.
+ * 
+ * @param {string | undefined} language - The languge the menus should be in. Can only be "sv" for Swedish, "fi" for Finnish and "en" for English. Swedish is default.
+ * 
+ * @returns {ITaffaMenu[]} An array with objects containing the different lunch alternatives for today plus four days into the future (not including Saturdays and Sundays). In addition, the objects will also contain the corresponding date.
+ */
 export const week = async (language?: keyof typeof TaffaLanguageEnum): Promise<ITaffaMenu[]> => {
   return rp(`${taffaBaseURL}/${language ? language : "sv"}/json/week/`)
     .then((body: string) => {
